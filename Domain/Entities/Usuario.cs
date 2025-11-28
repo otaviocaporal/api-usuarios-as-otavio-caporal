03c65ad17566791326715d83f76f0f40f3ca666b
otavio <otaviocaporal@gmail.com>
2025-11-27 22:29:19 -0300

    API Usuarios - estrutura base do projeto criada

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public  class  Usuario
+
+    {
+    [Key]
+    public  int Id { get; set; } // PK, Auto-increment
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public  string Nome { get; set; } = string.Empty; // Obrigatório, 3-100 caracteres
+
+    [Required]
+    [EmailAddress]
+    public  string Email { get; set; } = string.Empty; // Obrigatório, formato válido, único
+
+    [Required]
+    [MinLength(6)]
+    public  string Senha { get; set; } = string.Empty;// Obrigatório, min 6 caracteres
+
+    [Required]
+    public  DateTime DataNascimento { get; set; } // Obrigatório, idade >= 18 anos
+
+    [Phone]
+    [MaxLength(15)]
+    public  string Telefone { get; set; } // Opcional, formato (XX) XXXXX-XXXX
+
+    [Required]
+    public  bool Ativo { get; set; } // Obrigatório, default true
+
+    [Required]
+    public  DateTime DataCriacao { get; set; } = DateTime.Now; // Obrigatório, preenchido automaticamente
+
+    public  DateTime? DataAtualizacao { get; set; } // Opcional, atualizado automaticamente
+
+    }
+}
